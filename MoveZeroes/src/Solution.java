750afd6f2e995e8bcf81f747e358a308c0dce511
al <apizano@purdue.edu>
2020-04-04 18:44:31 -0500

    adding sysouts

--- MoveZeroes/src/Solution.java
+++ MoveZeroes/src/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
         int dynamic_length = nums.length-1;
         int temp = 0;
 
-        for(int i=0; i<nums.length; i++) {
+        for(int i=0; i<dynamic_length; i++) {
             if(nums[i] == 0) {
                 // store value in temp var before its erased
                 temp = nums[placeholder];
@@ -17,7 +17,7 @@ public class Solution {
 
                 //decrement placeholder
                 placeholder++;
-//                dynamic_length--;
+                dynamic_length--;
             }
         }
     }
@@ -37,9 +37,11 @@ public class Solution {
         for(Integer i: nums) {
             System.out.print(i + " ");
         }
+        System.out.println();
         for(Integer i: nums2) {
             System.out.print(i + " ");
         }
+        System.out.println();
         for(Integer i: nums3) {
             System.out.print(i + " ");
         }
