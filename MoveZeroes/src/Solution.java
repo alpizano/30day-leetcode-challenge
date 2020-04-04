b012a8251db0a4610926af4097070116b61250d7
al <apizano@purdue.edu>
2020-04-04 16:37:42 -0500

    adding main method

--- MoveZeroes/src/Solution.java
+++ MoveZeroes/src/Solution.java
@@ -2,4 +2,10 @@ public class Solution {
     public void moveZeroes(int[] nums) {
 
     }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        
+    }
 }
