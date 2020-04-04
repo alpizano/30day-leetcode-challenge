27703600150fc6a7dca68df2c0d54dcee570fe85
al <apizano@purdue.edu>
2020-04-04 18:41:07 -0500

    correcting algorithm

--- MoveZeroes/src/Solution.java
+++ MoveZeroes/src/Solution.java
@@ -1,23 +1,23 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
-        int placeholder = nums.length-1;
+        int placeholder = 1;
         int dynamic_length = nums.length-1;
         int temp = 0;
 
-        for(int i=0; i<dynamic_length; i++) {
+        for(int i=0; i<nums.length; i++) {
             if(nums[i] == 0) {
                 // store value in temp var before its erased
                 temp = nums[placeholder];
 
-                //copy 0 to end
+                //copy 0 to right
                 nums[placeholder] = nums[i];
 
                 // copy temp to 0's position
                 nums[i] = temp;
 
                 //decrement placeholder
-                placeholder--;
-                dynamic_length--;
+                placeholder++;
+//                dynamic_length--;
             }
         }
     }
@@ -26,11 +26,18 @@ public class Solution {
         // sample nums array inputs
         int[] nums = new int[]{0,1,0,3,12};
         int[] nums2 = new int[]{1,3,12,0,0};
+        int[] nums3 = new int[]{0,1,0,3,12};
 
         Solution sol = new Solution();
 
-        sol.moveZeroes(nums2);
+        sol.moveZeroes(nums3);
+        for(Integer i: nums) {
+            System.out.print(i + " ");
+        }
         for(Integer i: nums2) {
+            System.out.print(i + " ");
+        }
+        for(Integer i: nums3) {
             System.out.print(i + " ");
         }
     }
