b14f12a0311b46b7eea72b1fa09e798715218def
al <apizano@purdue.edu>
2020-04-04 16:40:27 -0500

    added second sample array input for testing

--- MoveZeroes/src/Solution.java
+++ MoveZeroes/src/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] nums = new int[]{0,1,0,3,12};
+        int[] nums2 = new int[]{1,3,12,0,0};
 
         Solution sol = new Solution();
 
