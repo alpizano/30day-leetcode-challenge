defcae483cfa09ae7b79492213a976e3df09c7b1
al <apizano@purdue.edu>
2020-04-04 18:08:25 -0500

    testing algorithm

--- MoveZeroes/src/Solution.java
+++ MoveZeroes/src/Solution.java
@@ -6,9 +6,16 @@ public class Solution {
         for(int i=0; i<nums.length; i++) {
             if(nums[i] == 0) {
                 // store value in temp var before its erased
-                
+                temp = nums[placeholder];
+
                 //copy 0 to end
-                nums[placeholder] =
+                nums[placeholder] = nums[i];
+
+                // copy temp to 0's position
+                nums[i] = temp;
+
+                //decrement placeholder
+                placeholder--;
             }
 
         }
@@ -23,5 +30,8 @@ public class Solution {
         Solution sol = new Solution();
 
         sol.moveZeroes(nums);
+        for(Integer i: nums) {
+            System.out.print(i + " ");
+        }
     }
 }
