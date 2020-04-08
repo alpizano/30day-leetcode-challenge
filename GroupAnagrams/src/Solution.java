f071fa6d92abe5a74881da55561cbf0fe667cc2f
al <apizano@purdue.edu>
2020-04-07 21:31:23 -0500

    creating Big List to hold List of List of Strings

--- GroupAnagrams/src/Solution.java
+++ GroupAnagrams/src/Solution.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -5,7 +6,8 @@ import java.util.List;
 
 public class Solution {
    public List<List<String>> groupAnagrams(String[] strs) {
-
+       boolean found_anagram = false;
+        List<List<String>> big_list = new ArrayList();
 
        for(int i=0; i<strs.length; i++) {
            // Create the list that will hold each anagram group
@@ -19,9 +21,13 @@ public class Solution {
                Arrays.sort(temp);
 
                // Do comparison
-               if(Arrays.equals(strs[i].toCharArray(),temp) {
-
+               if(Arrays.equals(strs[i].toCharArray(),temp)) {
+                    anagram_list.add(strs[j]);
                }
+           }
+           // Save the first item
+           if(found_anagram == true) {
+               anagram_list.add(strs[i]);
            }
        }
    }
