fc95a55127a67430b56e9f9b3f1e3990b5d63acc
al <apizano@purdue.edu>
2020-04-07 20:26:28 -0500

    starting Group Anagrams problem

---
+++ GroupAnagrams/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
