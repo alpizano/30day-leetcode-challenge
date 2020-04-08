6a97bee09aa0bbfc80c1aedc932d29378be07a13
al <apizano@purdue.edu>
2020-04-07 21:02:34 -0500

    implementing algorithm using list and loops

--- GroupAnagrams/src/Solution.java
+++ GroupAnagrams/src/Solution.java
@@ -1,15 +1,38 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+   //public List<List<String>> groupAnagrams(String[] strs) {
 
-        List<String> str_list = Arrays.asList(strs);
 
-        
 
-    }
+        // Convert the array to List so we can sort
+        //List<String> str_list = Arrays.asList(strs);
+//       List<>
+//
+//       for(int i=0; i<strs.length; i++) {
+//           for(int j=0; j< strs[j].length(); j++) {
+//               strs[j].charAt(j);
+//           }
+//       }
+//
+//        // Iterate
+//        for(String a: str_list) {
+//            for(String b: str_list) {
+////                if(a.equals(Collections.sort(b))){
+////
+////                }
+//
+//            }
+ //       }
+
+
+
+
+
+ //   }
 
     public static void main(String[] args) {
         String[] input = new String[]{"eat","tea","tan","ate","nat","bat"};
