808940c54c00607d310b391286492f43e7abbfcb
al <apizano@purdue.edu>
2020-04-07 20:23:39 -0500

    implementing algorith for buy and sell stocks

--- BestTimeBuySellStockII/src/Solution.java
+++ BestTimeBuySellStockII/src/Solution.java
@@ -1,9 +1,14 @@
 public class Solution {
     public int maxProfit(int[] prices) {
+        boolean buy = false;
+        boolean sell = false;
+
         return 0;
     }
 
     public static void main(String[] args) {
+        int[] int_array = new int[]{7,1,5,3,6,4};
+
         System.out.println("Hello World");
     }
 }
