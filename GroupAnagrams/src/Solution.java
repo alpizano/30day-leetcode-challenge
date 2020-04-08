f8a50a43ca413af0756aaef61940f4cf69f52944
al <apizano@purdue.edu>
2020-04-07 20:32:34 -0500

    implementing algorithm

--- GroupAnagrams/src/Solution.java
+++ GroupAnagrams/src/Solution.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
+        List<String> str_list = Arrays.asList(strs);
+
+        
+
     }
 
     public static void main(String[] args) {
+        String[] input = new String[]{"eat","tea","tan","ate","nat","bat"};
+
         System.out.println("Hello world");
     }
 
