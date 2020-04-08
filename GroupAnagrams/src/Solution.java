ed7717607fbe50e9800660384ec44aafe718b598
al <apizano@purdue.edu>
2020-04-07 22:30:47 -0500

    formatting and comments

--- GroupAnagrams/src/Solution.java
+++ GroupAnagrams/src/Solution.java
@@ -3,19 +3,20 @@ import java.util.*;
 
 public class Solution {
    public List<List<String>> groupAnagrams(String[] strs) {
+       // Setup
        boolean found_anagram = false;
-        List<List<String>> big_list = new ArrayList();
+       List<List<String>> big_list = new ArrayList();
        Hashtable<String, Integer> memory = new Hashtable();
-       ArrayList<Integer> seen_indices = new ArrayList();
        char[] temp = {};
        char[] clone = {};
 
        for(int i=0; i<strs.length; i++) {
-           // Create the list that will hold each anagram group
             found_anagram = false;
 
+           // Create the list that will hold each anagram group
            List<String> anagram_list = new ArrayList();
 
+           // Convert the String to a temporary char[] array so it can be sorted
            clone = strs[i].toCharArray();
            Arrays.sort(clone);
 
@@ -23,35 +24,36 @@ public class Solution {
 
                // Convert the String to a temporary char[] array so it can be sorted
                temp = strs[j].toCharArray();
-
-
-               // Sort the char[] array
                Arrays.sort(temp);
 
+               // Compare to see if Anagram if have not seen value before
                if(!memory.containsKey(String.valueOf(clone))) {
-
-                       // Do comparison
+                       // If Anagram, add jth value to List<Strings>
                        if (Arrays.equals(clone, temp)) {
                            anagram_list.add(strs[j]);
                        }
 
                }
+               // If we've seen this value before, skip it and don't add to List
                else {
                    found_anagram = true;
                    break;
                }
 
            }
-           // Save the first item
-           // Store into memoryt
-           System.out.println("Placing " + String.valueOf(clone) + " into Hashtable");
+
+           //System.out.println("Placing " + String.valueOf(clone) + " into Hashtable");
+
+           // Store ith value into "memory"
            memory.put(String.valueOf(clone),i);
+
+           // If we haven't seen the value before, add ith value to List<String>, then add entire List<String> to List<List<Strings>>
            if(found_anagram == false) {
                anagram_list.add(strs[i]);
                big_list.add(anagram_list);
            }
 
-           System.out.println("Current list" + big_list);
+           //System.out.println("Current list" + big_list);
        }
 
 //       for(String s: memory.keySet()) {
