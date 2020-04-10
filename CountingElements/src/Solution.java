f50e4a87782c314398d40e1c7439fe11688747ea
al <apizano@purdue.edu>
2020-04-09 22:51:33 -0500

    added main method and countElements method

--- CountingElements/src/Solution.java
+++ CountingElements/src/Solution.java
@@ -1,3 +1,10 @@
 public class Solution {
-    
+    public int countElements(int[] arr) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
 }
