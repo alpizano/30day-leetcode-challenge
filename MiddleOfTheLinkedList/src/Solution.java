9fdc83f1bd0e644e33a12286116bda4c1690b873
al <apizano@purdue.edu>
2020-04-09 22:20:27 -0500

    solving algorithm

--- MiddleOfTheLinkedList/src/Solution.java
+++ MiddleOfTheLinkedList/src/Solution.java
@@ -8,10 +8,27 @@ public class Solution {
         }
     }
     public ListNode middleNode(ListNode head) {
-        return null;
+        int count= 0;
+        ListNode ans = new ListNode(0);
+
+        for(ListNode curr = head.next; curr != null; curr = curr.next) {
+            count++;
+        }
+
+        int middle = (count/2) + 1;
+
+        count = 0;
+        for(ListNode curr = head.next; curr!= null; curr= curr.next) {
+            if(count == middle) {
+                ans = curr;
+            }
+            count++;
+        }
+
+        return ans;
     }
 
-    public static void main(String[]) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
