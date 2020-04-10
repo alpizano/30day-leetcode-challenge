84a3ba094a80ac0526cf1c99df68dc6cbbaca782
al <apizano@purdue.edu>
2020-04-09 21:26:34 -0500

    Revised solution to use Hashtable with nested List as values and String keys

--- GroupAnagrams/src/Solution.java
+++ GroupAnagrams/src/Solution.java
@@ -1,75 +1,60 @@
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+
 import java.lang.reflect.Array;
 import java.util.*;
 
 public class Solution {
-   public List<List<String>> groupAnagrams(String[] strs) {
-       // Setup
-       boolean found_anagram = false;
-       List<List<String>> big_list = new ArrayList();
-       Hashtable<String, Integer> memory = new Hashtable();
-       char[] temp = {};
-       char[] clone = {};
+    public List<List<String>> groupAnagrams(String[] strs) {
+    //public void groupAnagrams(String[] strs) {
+        // Setup
+        List<List<String>> anagram_list = new ArrayList();
+        List<String> inner_list = new ArrayList<>();
+        Hashtable<String, List<String>> memory = new Hashtable();
+      // HashSet<String> memory = new HashSet<>();
+       String[] temp = {};
+        String[] sorted_array = new String[strs.length];
 
-       for(int i=0; i<strs.length; i++) {
-            found_anagram = false;
 
-           // Create the list that will hold each anagram group
-           List<String> anagram_list = new ArrayList();
+        List<String> temp_list = new ArrayList<>();
 
-           // Convert the String to a temporary char[] array so it can be sorted
-           clone = strs[i].toCharArray();
-           Arrays.sort(clone);
+        for(int i=0; i<strs.length; i++) {
+            temp = strs[i].split("");
+            Arrays.sort(temp);
+            String sorted = String.join("", temp);
 
-           for(int j=i+1; j<strs.length; j++) {
+            // If value is already a key in Hashtable
+            if(memory.containsKey(sorted)) {
+               temp_list =  memory.get(sorted);
+               temp_list.add(strs[i]);
+            }
+            else {
+                // create the List
+                List<String> new_list = new ArrayList<>();
+                new_list.add(strs[i]);
+                memory.put(sorted, new_list);
+            }
+        }
 
-               // Convert the String to a temporary char[] array so it can be sorted
-               temp = strs[j].toCharArray();
-               Arrays.sort(temp);
+        for(String key: memory.keySet()) {
+            anagram_list.add(memory.get(key));
+        }
 
-               // Compare to see if Anagram if have not seen value before
-               if(!memory.containsKey(String.valueOf(clone))) {
-                       // If Anagram, add jth value to List<Strings>
-                       if (Arrays.equals(clone, temp)) {
-                           anagram_list.add(strs[j]);
-                       }
 
-               }
-               // If we've seen this value before, skip it and don't add to List
-               else {
-                   found_anagram = true;
-                   break;
-               }
-
-           }
-
-           //System.out.println("Placing " + String.valueOf(clone) + " into Hashtable");
-
-           // Store ith value into "memory"
-           memory.put(String.valueOf(clone),i);
-
-           // If we haven't seen the value before, add ith value to List<String>, then add entire List<String> to List<List<Strings>>
-           if(found_anagram == false) {
-               anagram_list.add(strs[i]);
-               big_list.add(anagram_list);
-           }
-
-           //System.out.println("Current list" + big_list);
-       }
-
-//       for(String s: memory.keySet()) {
-//           System.out.println(s);
-//       }
-       return big_list;
-   }
+     return anagram_list;
+    }
 
     public static void main(String[] args) {
-       Solution sol = new Solution();
+        Solution sol = new Solution();
         String[] input = new String[]{"eat","tea","tan","ate","nat","bat"};
+        String[] input2 = new String[]{"",""};
         List<List<String>> answer = new ArrayList();
 
         answer = sol.groupAnagrams(input);
-
+        //sol.groupAnagrams(input);
+       System.out.println(answer);
+        answer = sol.groupAnagrams(input2);
         System.out.println(answer);
+
     }
 
 }
