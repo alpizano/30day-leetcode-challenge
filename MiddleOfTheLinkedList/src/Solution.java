cac156e19c3132a9376a6bced5056a683a55103c
al <apizano@purdue.edu>
2020-04-09 21:34:57 -0500

    Added data class for ListNode and method body

--- MiddleOfTheLinkedList/src/Solution.java
+++ MiddleOfTheLinkedList/src/Solution.java
@@ -1,4 +1,18 @@
 public class Solution {
-    
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    public ListNode middleNode(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[]) {
+        System.out.println("Hello world");
+    }
 }
 
