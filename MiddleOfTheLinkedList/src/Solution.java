8a0474e5c34d334a77f520d77f22c54aee4c4004
al <apizano@purdue.edu>
2020-04-09 22:48:28 -0500

    successful submission for middle of linked list

--- MiddleOfTheLinkedList/src/Solution.java
+++ MiddleOfTheLinkedList/src/Solution.java
@@ -7,49 +7,47 @@ class ListNode {
     }
 }
 
-    class Solution {
+class Solution {
     public ListNode middleNode(ListNode head) {
-            int count = 0;
-            int middle = 0;
-            ListNode ans = new ListNode(0);
+        int count = 0;
+        int middle = 0;
+        ListNode ans = new ListNode(0);
 
-            for (ListNode curr = head; curr != null; curr = curr.next) {
-                count++;
-            }
-
-            if (count % 2 == 0) {
-                middle = (count/2) + 1;
-            }else {
-                middle = count / 2;
-            }
-            count = 0;
-
-            for (ListNode curr = head; curr != null; curr = curr.next) {
-                if (count == middle) {
-                    ans = curr;
-                }
-                count++;
-            }
-
-            return ans;
+        for (ListNode curr = head; curr != null; curr = curr.next) {
+            count++;
         }
 
-        public static void main(String[] args) {
-            System.out.println("Hello world");
-            Solution sol = new Solution();
-            ListNode head = new ListNode(1);
-            ListNode ans;
+        middle = count / 2;
 
-            // List of [ 1, 2, 3, ,4 ,5]
-            head.next = new ListNode(2);
-            head.next.next = new ListNode(3);
-            head.next.next.next = new ListNode(4);
-            head.next.next.next.next = new ListNode(5);
-            ans = sol.middleNode(head);
-            System.out.println(ans.val);
+        count = 0;
 
+        for (ListNode curr = head; curr != null; curr = curr.next) {
+            if (count == middle) {
+                ans = curr;
+            }
+            count++;
         }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Solution sol = new Solution();
+        ListNode head = new ListNode(1);
+        ListNode ans;
+
+        // List of [ 1, 2, 3, 4, 5, 6]
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        ans = sol.middleNode(head);
+        System.out.println(ans.val);
 
     }
+
+}
 
 
