7456586068a48fbec690bd96e0d5dc52b76cb589
al <apizano@purdue.edu>
2020-04-15 22:30:13 -0500

    brute force recursion solution buy and sell stock

--- BestTimeBuySellStockII/src/Solution.java
+++ BestTimeBuySellStockII/src/Solution.java
@@ -1,25 +1,36 @@
+import java.util.HashMap;
+
 public class Solution {
     public int maxProfit(int[] prices) {
-        boolean buy = false;
-        boolean sell = false;
+        return calculate(prices, 0);
+    }
 
-        return 0;
+    public int calculate(int prices[], int s) {
+        if (s >= prices.length)
+            return 0;
+        int max = 0;
+        for (int start = s; start < prices.length; start++) {
+            int maxprofit = 0;
+            for (int i = start + 1; i < prices.length; i++) {
+                if (prices[start] < prices[i]) {
+                    int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+                    if (profit > maxprofit)
+                        maxprofit = profit;
+                }
+            }
+            if (maxprofit > max)
+                max = maxprofit;
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         int[] intArray = new int[]{7,1,5,3,6,4};
         int[] differenceArray = new int[intArray.length-1];
+        int max =0;
 
+        Solution sol = new Solution();
 
-        for(int i=0; i+1<intArray.length; i++) {
-            differenceArray[i] = intArray[i+1] - intArray[i];
-        }
-
-        for(int i: differenceArray) {
-            System.out.print(i + " ");
-        }
-        //System.out.println(differenceArray.length);
-
-        System.out.println("Hello World");
+        sol.maxProfit(intArray);
     }
 }
