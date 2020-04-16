68ec1a2577eee6b313f06ba6170bd20bb41cde5e
al <apizano@purdue.edu>
2020-04-15 21:29:09 -0500

    creating difference array to hold diferences

--- BestTimeBuySellStockII/src/Solution.java
+++ BestTimeBuySellStockII/src/Solution.java
@@ -7,7 +7,18 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] int_array = new int[]{7,1,5,3,6,4};
+        int[] intArray = new int[]{7,1,5,3,6,4};
+        int[] differenceArray = new int[intArray.length-1];
+
+
+        for(int i=0; i+1<intArray.length; i++) {
+            differenceArray[i] = intArray[i+1] - intArray[i];
+        }
+
+        for(int i: differenceArray) {
+            System.out.print(i + " ");
+        }
+        //System.out.println(differenceArray.length);
 
         System.out.println("Hello World");
     }
