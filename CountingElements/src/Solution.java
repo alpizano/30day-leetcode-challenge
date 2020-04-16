3bb76785eb7c89e31cdb5dd854ae551301de885b
al <apizano@purdue.edu>
2020-04-15 20:14:27 -0500

    coding algorithm using java 8 streams

--- CountingElements/src/Solution.java
+++ CountingElements/src/Solution.java
@@ -1,10 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
     public int countElements(int[] arr) {
+
         return 0;
+    }
+
+    public void mapStore() {
+
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        List<Integer> arr = new ArrayList(Arrays.asList(1,1,3,3,5,5,7,7));
+
+        arr.stream().forEach((x) -> System.out.print(x));
     }
 
 }
