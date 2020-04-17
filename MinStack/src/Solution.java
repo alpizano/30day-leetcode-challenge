fbc6f131aea2a614005b356d255e93bc8b1c9d7b
al <apizano@purdue.edu>
2020-04-17 00:49:50 -0500

    finiished minstack problem using custom LinkedList and List for memory of min value

--- MinStack/src/Solution.java
+++ MinStack/src/Solution.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,21 +20,23 @@ class MinStack {
     int min;
 
     public MinStack() {
+        head = null;
         valList = new LinkedList<>();
         min = 0;
     }
 
     public void push(int x) {
-        // Add new value to values list
+        // Add new value to values List
         valList.add(x);
-        // Sort the values to obtain minimum
 
         // Pushes val in-front of current node; After head
         head = new ListNode(x, head);
     }
 
     public void pop() {
+        valList.remove(valList.size()-1);
         head = head.next;
+
     }
 
     public int top() {
@@ -40,22 +44,29 @@ class MinStack {
     }
 
     public int getMin() {
-        return this.min;
+        // Create temp sorted list
+        List<Integer> sortedList = new ArrayList<>(valList);
+        // Sorts in ascending order
+        Collections.sort(sortedList);
+
+        // Index 0 will have the lowest value
+        return sortedList.get(0);
     }
 
-    public void print() {
-        System.out.println("Printing out contents of linked list:");
-        for (ListNode cursor = head; cursor != null; cursor = cursor.next) {
-            System.out.print(cursor.val + " ");
-        }
-        System.out.println();
-        Collections.sort(valList);
-        System.out.println("Printing out contents of minimum values list:");
-        for (Integer i : valList) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
+//    public void print() {
+//        System.out.println("Printing out contents of linked list:");
+//        for (ListNode cursor = head; cursor != null; cursor = cursor.next) {
+//            System.out.print(cursor.val + " ");
+//        }
+//        System.out.println();
+//
+//        Collections.sort(valList);
+//        System.out.println("Printing out contents of minimum values list:");
+//        for (Integer i : valList) {
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//    }
 
 }
 
@@ -66,11 +77,14 @@ public class Solution {
         minStack.push(-2);
         minStack.push(0);
         minStack.push(-3);
-        minStack.print();
-        System.out.println(minStack.getMin()); // Should return -3
+        //System.out.println(minStack.top()); // Should return 0
+
+       //minStack.print();
+
+        System.out.println("Return -3: " + minStack.getMin()); // Should return -3
         minStack.pop();
-        System.out.println(minStack.top()); // Should return 0
-        System.out.println(minStack.getMin()); // Should return -2
+        System.out.println("Return 0: " + minStack.top()); // Should return 0
+        System.out.println("Return -2: " + minStack.getMin()); // Should return -2
 
         //minStack.print();
     }
