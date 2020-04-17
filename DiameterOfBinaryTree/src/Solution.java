637a93276ddc85de0c60c75d695bedc4d7f2f24f
al <apizano@purdue.edu>
2020-04-17 00:52:50 -0500

    Starting diameter of binary tree problem

---
+++ DiameterOfBinaryTree/src/Solution.java
@@ -0,0 +1,20 @@
+  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+ }
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
