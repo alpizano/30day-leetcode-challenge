0acdb598326463a27980004f09cf772f183b899e
al <apizano@purdue.edu>
2020-04-17 23:54:27 -0500

    implementing algorith to get longest path from left and right nof root and sum path

--- DiameterOfBinaryTree/src/Solution.java
+++ DiameterOfBinaryTree/src/Solution.java
@@ -10,8 +10,28 @@
 
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
+
+
         return 0;
     }
+
+    public int getLeftmostPath(TreeNode root) {
+        TreeNode cursor = root;
+        int length= 0;
+
+        while(cursor != null) {
+            if(cursor.left == null) {
+                cursor = cursor.right;
+                length++;
+            }
+            else if(cursor.right == null) {
+                cursor = cursor.left;
+                length++;
+            }
+        }
+    }
+
+
 
     public static void main(String[] args) {
         //            1
@@ -20,9 +40,14 @@ class Solution {
         //         / \
         //        4   5
 
-        TreeNode tree = new TreeNode(1);
-        tree.left = new TreeNode(2);
-        tree.right = new TreeNode(3);
+        // Creating sample tree structure from above input
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(root.val);
     }
 
 }
